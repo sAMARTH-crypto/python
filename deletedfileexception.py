d58bae2f1a838a407155f5d6ebbad86f74717537
sAMARTH-crypto <51755501+sAMARTH-crypto@users.noreply.github.com>
2019-06-13 09:08:45 -0700

    Update deletedfileexception.py

--- deletedfileexception.py
+++ deletedfileexception.py
@@ -12,3 +12,17 @@ if os.path.exists('sample2.txt'):
    data.close() 
 else:
     print('file is missing')
+      
+      
+ #or TRY AND CATCH
+ try:  
+      data = open('sketch.txt')
+      for each_line in data:     
+         try:          
+            (role, line_spoken) = each_line.split(':', 1)        
+            print(role, end='')          print(' said: ', end='')     
+            print(line_spoken, end='')  
+            except:    pass
+      data.close() 
+except:
+   print('The data file is missing!')
