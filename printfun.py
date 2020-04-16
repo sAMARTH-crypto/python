aff329c8653c96b98e448de9fcf8f175d87fca4d
sAMARTH-crypto <51755501+sAMARTH-crypto@users.noreply.github.com>
2020-04-16 09:55:15 +0530

    Comment

--- printfun.py
+++ printfun.py
@@ -1,7 +1,7 @@
 #Print Function
 
 N=int(input())
-m=str(N)             #here string valuse of N is stored in m which will be concatenated to our output
+m=str(N)             #here of N is stored as a string in m which will be concatenated to our output
 for i in range(1,N,1):
     j=N-i               #here we are subtracting value of i from N  and storing it as string in m to get desired output
     m=str(j) + m            #string concatenation
